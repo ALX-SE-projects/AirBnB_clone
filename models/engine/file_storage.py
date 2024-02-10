db632f2a68aa73f18e046f4d4875f3a4b6c7060c
ibrahemesam <iimosa7777@gmail.com>
2024-02-10 21:35:24 +0200

    hema edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,13 +7,8 @@ import os
 
 class FileStorage:
     "serializes instances to a JSON file and deserializes JSON file to instances"
-    
-    def __init__(self, *args, **kwargs):
-        if 'file_path' in kwargs.keys():
-            self.__file_path = kwargs['file_path']
-        else:
-            self.__file_path = 'file.json'
-        self.__objects = {}
+    __file_path = 'file.json'
+    __objects = {}
         
     def all(self):
         "returns the dictionary __objects"
