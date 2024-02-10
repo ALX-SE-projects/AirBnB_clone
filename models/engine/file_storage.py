0bb3a1882044ade7a062af816166dc8081e1f26b
ibrahemesam <iimosa7777@gmail.com>
2024-02-10 22:22:33 +0200

    blabla bla

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,8 +42,3 @@ class FileStorage:
         # else:
         #     self.save()
           
-              
-import sys
-if sys.argv[0].startswith('file_storage_'):
-    print('OK')
-    os._exit(0)
