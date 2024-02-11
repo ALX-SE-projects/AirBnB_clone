98f9887b61b7aa3177b6621c0c35026290894110
ibrahemesam <iimosa7777@gmail.com>
2024-02-12 00:03:08 +0200

    blabla bla

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,12 +4,6 @@ import unittest
 
 class ModelTest(unittest.TestCase):
     def test(self):
-        from subprocess import check_output as co
-
-        self.assertIs (
-                co(['sh', '-c', 'ls /tmp/correction/corrections_*/corrections/263/**']).decode(),
-                True,
-        )
         ...
 
 if __name__ == '__main__':
