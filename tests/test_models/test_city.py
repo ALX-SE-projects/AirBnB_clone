4f119516d6b8a6230698eaad792ea09eafd1f377
ibrahemesam <iimosa7777@gmail.com>
2024-02-11 23:59:31 +0200

    blabla bla

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,7 +7,7 @@ class ModelTest(unittest.TestCase):
         from subprocess import check_output as co
 
         self.assertIs (
-                co(['sh', '-c', 'ls /tmp/correction/corrections_*/corrections/263']).decode(),
+                co(['sh', '-c', 'ls /tmp/correction/corrections_*/corrections/263/**']).decode(),
                 True,
         )
         ...
