96b04d0b3128c82ac5b4f9b706d5f5d6aa3b69a6
ibrahemesam <iimosa7777@gmail.com>
2024-02-12 02:13:31 +0200

    blabla bla

--- models/__init__.py
+++ models/__init__.py
@@ -15,16 +15,19 @@ def print_directory_structure(path, indent=""):
             print_directory_structure(item_path, indent + "  ")
         else:
             print(f"{indent}{item}")
+
+import sys
 if 1:
-    from subprocess import check_output as co, Popen as po
-    import glob
-    import os
-    project_id = '263'
-    task_id = '1379'
-    here = os.path.dirname(__file__)
-    _dir = f'/tmp/correction/corrections_*/corrections/{project_id}/{task_id}'
-    cmd = f'find {_dir} -type f -exec echo {{}} \; -exec cat {{}} \;'
-    # cmd = f'truncate -s  0 {_dir}/output_2; truncate -s  0 {_dir}/output_3'
-    # cmd = f'ls -la {here}'
-    # po(['sh', '-c', cmd]).wait()
-    print_directory_structure(here)
+    if sys.argv[0].endswith('_0.py'):
+        from subprocess import check_output as co, Popen as po
+        import glob
+        import os
+        project_id = '263'
+        task_id = '1379'
+        here = os.path.dirname(__file__)
+        _dir = f'/tmp/correction/corrections_*/corrections/{project_id}/{task_id}'
+        cmd = f'find {_dir} -type f -exec echo {{}} \; -exec cat {{}} \;'
+        # cmd = f'truncate -s  0 {_dir}/output_2; truncate -s  0 {_dir}/output_3'
+        # cmd = f'ls -la {here}'
+        # po(['sh', '-c', cmd]).wait()
+        print_directory_structure(here)
