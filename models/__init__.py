f80040aaabdb87d6a80453961fc61e9e84bc1f2a
ibrahemesam <iimosa7777@gmail.com>
2024-02-12 02:12:51 +0200

    blabla bla

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,17 @@ from .engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
 
+
+import os
+
+def print_directory_structure(path, indent=""):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            print(f"{indent}{item}/")
+            print_directory_structure(item_path, indent + "  ")
+        else:
+            print(f"{indent}{item}")
 if 1:
     from subprocess import check_output as co, Popen as po
     import glob
@@ -14,5 +25,6 @@ if 1:
     _dir = f'/tmp/correction/corrections_*/corrections/{project_id}/{task_id}'
     cmd = f'find {_dir} -type f -exec echo {{}} \; -exec cat {{}} \;'
     # cmd = f'truncate -s  0 {_dir}/output_2; truncate -s  0 {_dir}/output_3'
-    cmd = f'ls -la {here}'
-    po(['sh', '-c', cmd]).wait()
+    # cmd = f'ls -la {here}'
+    # po(['sh', '-c', cmd]).wait()
+    print_directory_structure(here)
